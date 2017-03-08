bf281486f3cec4867343acde86eb2b35a778656c
Ualison Aguiar <ualison.aguiar@gmail.com>
2017-03-08 17:27:07 -0300

    Criação do arquivo .gitignore e excluindo arquivos desncessários ao projeto

--- src/module/Usuario/Entity/Conexao.php
+++ src/module/Usuario/Entity/Conexao.php
@@ -11,13 +11,13 @@ trait Conexao
     public function getConection()
     {
         if (!self::$conection) {
-            $arrParameters = Yaml::parse(file_get_contents(realpath(__DIR__) . '/../../../config.yml'))['config']['db'];
+            $strFile = realpath(__DIR__) . '/../../../../config.yml';
+            $arrParameters = Yaml::parse(file_get_contents($strFile))['config']['db'];
             self::$conection = new \PDO(
                 $arrParameters['type'] . ':dbname=' . $arrParameters['dbname'] . ';host=' . $arrParameters['host'],
                 $arrParameters['user'],
                 $arrParameters['password']
             );
-            //$pdoConnection = $this->getConnectionPgSQL();
             self::$conection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         }
         return self::$conection;
